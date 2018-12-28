2f3ae941c1405af3489176133a6dbdcac0791da0
mshakya20 <mikeshshakya20@gmail.com>
2018-12-27 19:33:24 -0500

    adding file

---
+++ src/main/java/com/myblog/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.myblog.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myblog.entity.User;
+import com.myblog.service.UserService;
+
+@RestController
+@RequestMapping("/use")
+public class UserController {
+	@Autowired
+	private UserService userservice;
+	
+	@GetMapping("/allUsers")
+	public List<User>getAllUsers(){
+		return userservice.getAllUsers();
+	}
+	@PostMapping("/")
+	public void addUser(String name, int age) {
+		userservice.addUser(name,age);
+		
+	}
+	
+	
+	
+}
